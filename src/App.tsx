108cd7ed7237ef2ed63bb956de3065a3df5277e6
Serbe <mkoctuk@gmail.com>
2021-03-12 15:45:04 +0300

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 
-// import { TestNavBar } from './components/testnavbar';
 import { Nav } from './components/nav';
 
 const App = (): JSX.Element => {
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,8 +2,67 @@ import React, { useState, useRef } from 'react';
 import useOnClickOutside from "../utils/clickOutside";
 import './nav.css';
 
+// interface Setter {
+//   setter: (value: boolean) => void;
+// }
+
+// interface OpenState {
+//   open: boolean;
+//   setter: (value: boolean) => void;
+// }
+
+const mainItems = [
+  { link: '/contacts', name: 'Контакты' },
+  { link: '/companies', name: 'Организации' },
+  { link: '/sirens', name: 'Сирены' },
+];
+
+const dropdownItems = [
+  { link: '/departments', name: 'Отделы' },
+  { link: '/educations', name: 'Обучение' },
+  { link: '/kinds', name: 'Типы тренировок' },
+  { link: '/posts', name: 'Должности' },
+  { link: '/practices', name: 'Учения' },
+  { link: '/ranks', name: 'Чины' },
+  { link: '/scopes', name: 'Сферы' },
+  { link: '/certificates', name: 'Удостоверения' },
+  { link: '/sirentypes', name: 'Типы сирен' },
+];
+
+// const activeItem = "text-gray-900 bg-gray-200";
+// const passiveItem = "bg-transparent";
+
+const MainItems = (): JSX.Element => (
+  <>
+    {mainItems.map((item) => (
+      <a
+        className="nav-item md:ml-4"
+        href={item.link}
+        key={`main-items-${item.name}`}
+      >
+        {item.name}
+      </a>
+    ))}
+  </>
+);
+
+const DropdownItems = (): JSX.Element => (
+  <div className="px-2 py-2 bg-white rounded-md shadow">
+    {dropdownItems.map((item) => (
+      <a
+        className="block nav-item"
+        href={item.link}
+        key={`dropdown-items-${item.name}`}
+      >
+        {item.name}
+      </a>
+    ))}
+  </div>
+);
+
 export const Nav = (): JSX.Element => {
   const [open, setOpen] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
   const ref = useRef(null);
 
   const handleClickOutside = () => {
@@ -13,11 +72,11 @@ export const Nav = (): JSX.Element => {
   useOnClickOutside(ref, handleClickOutside);
 
   return (
-    <div ref={ref} className="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800">
+    <div ref={ref} className="w-full text-gray-700 bg-white">
       <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <div className="p-4 flex flex-row items-center justify-between">
-          <a href="#" className="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">ЕДДС</a>
-          <button className="md:hidden rounded-lg focus:outline-none focus:shadow-outline" onClick={() => setOpen(!open)}>
+          <a href="#" className="text-lg font-semibold tracking-widest text-gray-900 rounded-lg focus:outline-none focus:ring">ЕДДС</a>
+          <button className="md:hidden rounded-lg focus:outline-none focus:ring" onClick={() => setOpen(!open)}>
             <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
               <path className={open ? "hidden" : ""} fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clipRule="evenodd"></path>
               <path className={open ? "" : "hidden"} fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
@@ -25,23 +84,22 @@ export const Nav = (): JSX.Element => {
           </button>
         </div>
         <nav className={`${open ? "flex" : "hidden"} flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row`}>
-          <a className="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Blog</a>
-          <a className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Portfolio</a>
-          <a className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">About</a>
-          <a className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Contact</a>
+          <MainItems />
           <div className="relative">
-            <button onClick={() => setOpen(!open)} className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-              <span>Dropdown</span>
+            <button onClick={() => setOpen(!open)} className="flex flex-row items-center w-full nav-item text-left md:w-auto md:inline md:ml-4">
+              <span>Справочники</span>
               <svg fill="currentColor" viewBox="0 0 20 20" className={`${open ? "rotate-180" : "rotate-0"} inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1`}><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </button>
             <div x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" className={`${open ? "" : "hidden"} absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48`}>
-              <div className="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
-                <a className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #1</a>
-                <a className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #2</a>
-                <a className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #3</a>
-              </div>
+              <DropdownItems />
             </div>
-          </div>    
+          </div>
+          <div className="relative">
+              <button onClick={() => setOpenMenu(!openMenu)} className="flex flex-row items-center w-full nav-item text-left md:w-auto md:inline md:ml-4">
+              <span>Меню</span>
+              <svg fill="currentColor" viewBox="0 0 20 20" className={`${openMenu ? "rotate-180" : "rotate-0"} inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1`}><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+            </button>
+            </div>
         </nav>
       </div>
     </div>
